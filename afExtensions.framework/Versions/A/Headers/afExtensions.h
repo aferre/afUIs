1e62feff3217da5f7bc8ad51b72722743c20b79c
Ferre Adrien <ferre.adrien@gmail.com>
2011-09-30 16:07:14 +0200

    Added afExtensions Framework instead of classes

---
+++ afExtensions.framework/Versions/A/Headers/afExtensions.h
@@ -0,0 +1,13 @@
+//
+//  afExtensions.h
+//  afExtensions
+//
+//  Created by adrien ferré on 29/09/11.
+//  Copyright 2011 Ferré. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <afExtensions/afDateExtensions.h>
+#import <afExtensions/afStringExtensions.h>
+#import <afExtensions/afFileManagerExtensions.h>
+#import <afExtensions/aViewExtensions.h>
